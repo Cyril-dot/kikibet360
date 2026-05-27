0156db43b5a652adc86a750cd1c98d04db04a3e5
Ottih Chuks <chukwukaottih@gmail.com>
2026-05-27 14:02:14 +0000

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,7 +3,7 @@
 // Base URL: https://futballbackend.onrender.com
 // =============================================================================
 
-const BASE_URL = "https://futballbackend-production-7d3b.up.railway.app";
+const BASE_URL = "https://futballbackend-production-aefb.up.railway.app";
 // ---------------------------------------------------------------------------
 // Types & Schemas
 // ---------------------------------------------------------------------------
